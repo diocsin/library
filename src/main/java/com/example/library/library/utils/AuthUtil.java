c71abc72dc3f105518d099fe879fb613cf9f7be4
diocsin <30667165+diocsin@users.noreply.github.com>
2021-07-21 14:02:46 +0300

    Добавлен утилит класс для получения пользователя из контекста

---
+++ src/main/java/com/example/library/library/utils/AuthUtil.java
@@ -0,0 +1,48 @@
+package com.example.library.library.converter;
+
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+
+public class AuthUtil {
+
+    private AuthUtil() {
+    }
+
+    /**
+     * @return пользователь, аутентифицированный в текущем контексте безопасности или null.
+     */
+    public static UserDetails getUserFromContext() {
+        return getUser(getContext().getAuthentication());
+    }
+
+    /**
+     * @param authentication данные аутентификации пользователя
+     * @return пользователь
+     */
+    public static UserDetails getUser(final Authentication authentication) {
+        return authentication == null ? null : (UserDetails) authentication.getPrincipal();
+    }
+
+    /**
+     * Обновление принципала пользователя. Необходимо при изменении учётных данных текущего
+     * пользователя.
+     *
+     * @param user пользователь
+     */
+    public static void updatePrincipal(final UserDetails user) {
+        Authentication token = new UsernamePasswordAuthenticationToken(user, user.getPassword(),
+                user.getAuthorities());
+        getContext().setAuthentication(token);
+    }
+
+    /**
+     * @return the current security context (never null)
+     */
+    private static SecurityContext getContext() {
+        return SecurityContextHolder.getContext();
+    }
+
+}
